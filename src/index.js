686c526df568f6a77eb1fb8b41735536ba5372b7
zhang_yan <zhang_yan@gzhc365.com>
2019-01-28 15:38:17 +0800

    getting started

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+function component() {
+  let element = document.createElement('div');
+  element.innerHTML = _.join(['hello', 'YxY']);
+
+  return element;
+}
+
+document.body.appendChild(component());
