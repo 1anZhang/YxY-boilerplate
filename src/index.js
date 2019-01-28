db462aa6395bf086fab270842b95135125a7da6b
zhang_yan <zhang_yan@gzhc365.com>
2019-01-28 16:18:14 +0800

    output management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import YaYa from './avatar.jpg';
+import printMe from './print.js';
 
 function component() {
   let element = document.createElement('div');
@@ -11,6 +12,11 @@ function component() {
   myPic.src = YaYa;
   element.appendChild(myPic);
 
+  let btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+  
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -20,5 +26,12 @@ module.exports = {
         loader: 'file-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new ManifestPlugin()
+  ]
 };
