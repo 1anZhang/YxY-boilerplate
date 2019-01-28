4faaf03848121b07dc82bd4cd7361a160d917c51
zhang_yan <zhang_yan@gzhc365.com>
2019-01-28 16:04:57 +0800

    assert management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
+import './style.css';
+import YaYa from './avatar.jpg';
 
 function component() {
   let element = document.createElement('div');
-  element.innerHTML = _.join(['hello', 'YxY']);
+  element.innerHTML = _.join(['hello', 'YxY'], ' ');
+  element.classList.add('hello');
+
+  let myPic = new Image();
+  myPic.src = YaYa;
+  element.appendChild(myPic);
 
   return element;
 }
